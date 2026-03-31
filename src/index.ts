1867af37819cf078ec181ff444ab99696a4fb7b7
Bruno Costa <obrnocsta@gmail.com>
2026-03-31 17:21:27 -0300

    feat: adiciona ainda mais dados em arquivo separado, refaz o tipo 'Pets', adicionando novas chavez e tipos

--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,10 @@ import "dotenv/config";
 import express from "express";
 import type { Express } from "express";
 
+import { pets } from "./data/pets";
+
 const PORT = process.env.PORT || 8000;
 const app: Express = express();
-
-type Pet = {
-  name: string;
-  species: string;
-  adopted: boolean;
-  age: number;
-};
-
-const pets: Pet[] = [
-  {
-    name: "Augusto",
-    species: "dog",
-    adopted: true,
-    age: 1,
-  },
-  {
-    name: "Cesar",
-    species: "cat",
-    adopted: true,
-    age: 2,
-  },
-];
 
 app.get("/", (req, res) => {
   res.json(pets);
