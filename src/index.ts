fd830fda7b67752134429bf939b33abdec4c65fe
Bruno Costa <obrnocsta@gmail.com>
2026-03-31 16:55:24 -0300

    feat: adiciona um 'custom type' e adiciona tipo a um objeto 'pet', e envia como resposta 'json'

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,22 @@ import type { Express } from "express";
 const PORT = process.env.PORT || 8000;
 const app: Express = express();
 
+type Pet = {
+  name: string;
+  species: string;
+  adopted: boolean;
+  age: number;
+};
+
+const pet: Pet = {
+  name: "Augusto",
+  species: "dog",
+  adopted: true,
+  age: 1,
+};
+
 app.get("/", (req, res) => {
-  res.json({});
+  res.json(pet);
 });
 
 app.listen(PORT, (): void => {
